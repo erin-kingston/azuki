4919475c3a838241bdbe1301838a8032b4a673d6
erin-kingston <erin.kingston@anaplan.com>
2026-06-19 15:56:47 +0100

    SYSS-1160 Add helper functions for World

--- mondex/kazuki/src/main/kotlin/com/anaplan/engineering/azuki/mondex/kazuki/abs/World.kt
+++ mondex/kazuki/src/main/kotlin/com/anaplan/engineering/azuki/mondex/kazuki/abs/World.kt
@@ -60,9 +60,9 @@ class WorldFunctions(world: World) {
         },
         pre = { a, transferDetails ->
             abstractWorldSecureOperation.pre(a, transferDetails)
-                && transferDetails.from in world.authPurses.dom
-                && transferDetails.to in world.authPurses.dom
-                && world.authPurses[transferDetails.from].balance >= transferDetails.value
+                && authentic(transferDetails.from)
+                && authentic(transferDetails.to)
+                && sufficientFundsProperty(transferDetails)
                 && transferDetails.from != transferDetails.to
         },
         post = { a, transferDetails, result: World ->
@@ -84,9 +84,9 @@ class WorldFunctions(world: World) {
         },
         pre = { a, transferDetails ->
             abstractWorldSecureOperation.pre(a, transferDetails)
-                && transferDetails.from in world.authPurses.dom
-                && transferDetails.to in world.authPurses.dom
-                && world.authPurses[transferDetails.from].balance >= transferDetails.value
+                && authentic(transferDetails.from)
+                && authentic(transferDetails.to)
+                && sufficientFundsProperty(transferDetails)
                 && transferDetails.from != transferDetails.to
         },
         post = { a, transferDetails, result: World ->
@@ -96,4 +96,17 @@ class WorldFunctions(world: World) {
                 && result.authPurses[transferDetails.to] == world.authPurses[transferDetails.to]
         }
     )
+
+    private val authentic = function(
+        command = { name: Name ->
+            name in world.authPurses.dom
+        }
+    )
+
+    private val sufficientFundsProperty = function(
+        command = { transferDetails: TransferDetails ->
+            transferDetails.value <= world.authPurses[transferDetails.from].balance
+        }
+    )
+
 }
